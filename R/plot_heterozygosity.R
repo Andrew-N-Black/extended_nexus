f98d5c8bba8c372e3a15484cc0e851b1492b2a69
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2026-09-18 18:56:05 -0700

    Update plot_heterozygosity.R

--- R/plot_heterozygosity.R
+++ R/plot_heterozygosity.R
@@ -1,148 +1,37 @@
 #Load libraries
-library(readxl)
 library(ggplot2)
-
-#Read in metadata
-metadata <- read_xlsx("/Users/andrewblack/Documents/Research/GROUSE/sarek_nexus_new_plus_shotguns/heterozygosity_extended_nexus.xlsx")
-#Read in covariation matrix
-cov<-as.matrix(read.table("~/final.cov"))
-
-#Extract and calculate eplained variation
-axes<-eigen(cov)
-head(axes$values/sum(axes$values)*100)
-#[1] 22.6180474  2.9970624  2.1391689  0.6050842  0.5466266
-#[6]  0.4565761
-
-
-#Bind vectors with metadata and plot
-PC1_3<-as.data.frame(axes$vectors[,1:3])
-x<-cbind(PC1_3,metadata)
- #By species and group
-ggplot(data=x, aes(y=V2, x=V1)) +
-    geom_point(size=6, color="black", aes(shape=GROUP, fill=SPECIES)) +
-    theme_classic() +
-    xlab("PC1 (22.6%)") + ylab("PC2 (3.0%)") +
-    geom_hline(yintercept=0, linetype="dashed") +
-    geom_vline(xintercept=0, linetype="dashed") +
-    scale_fill_manual("Species", values=c("goldenrod","brown","black","grey")) +
-    scale_shape_manual("Group", values=c(25,21)) +
-    theme(legend.position = "right") +
-    guides(
-        fill = guide_legend(override.aes = list(shape=21, size=5, stroke=0.5)),
-        shape = guide_legend(override.aes = list(fill="grey50", size=5))
-    )+guides(
-        fill = guide_legend(override.aes = list(shape=21, size=5, stroke=0.5)),
-        shape = guide_legend(override.aes = list(fill=NA, size=5))
-    )
-
-
------------------------------------------------------------
-#Or 3-D plot
-#### Load libraries ####
 library(readxl)
+library(ggpubr)
+library(dplyr)
+
+#Load libraries
 library(ggplot2)
-library(plotly)
-library(reticulate)
+library(readxl)
+library(ggpubr)
+library(dplyr)
 
-#### Read in metadata ####
-metadata <- read_xlsx("/Users/andrewblack/Documents/Research/GROUSE/sarek_nexus_new_plus_shotguns/heterozygosity_extended_nexus.xlsx")
-#### Read in covariance matrix ####
-cov <- as.matrix(read.table("~/final.cov"))
+#By DPS and Species
+#load metadata
+HET_FILT <- read_xlsx("/Users/andrewblack/Documents/Research/GROUSE/sarek_nexus_new_plus_shotguns/heterozygosity_extended_nexus.xlsx")
+#Plot
+ggplot(HET_FILT, aes(x=SPECIES, y=heterozygosity, fill=SPECIES)) +geom_boxplot() +scale_fill_manual("", values=c("Tympanuchus cupido" = "goldenrod","Tympanuchus pallidicinctus"="brown","Tympanuchus phasianellus" = "black","Tympanuchus phasianellus/Tympanuchus cupido" = "grey")) + xlab("") + ylab("H") +theme_classic() +theme(axis.text.y = element_text(size=12)) +theme(legend.position="bottom") +theme(axis.text=element_text(size=14), axis.title=element_text(size=22, face="italic")) +theme(strip.text = element_text(size=18))+facet_wrap(~GROUP)+theme(axis.text.x = element_blank(), axis.title.x = element_blank())
 
-#### Extract and calculate explained variation ####
-axes <- eigen(cov)
-head(axes$values / sum(axes$values) * 100)
-# [1] 22.6180474  2.9970624  2.1391689  0.6050842  0.5466266
-# [6]  0.4565761
 
-#### Bind PC vectors with metadata ####
-PC1_3 <- as.data.frame(axes$vectors[, 1:3])
-x <- cbind(PC1_3, metadata)
 
-#### 2D ggplot: PC1 vs PC2, colored by SPECIES, shaped by GROUP ####
-ggplot(data = x, aes(y = V2, x = V1)) +
-  geom_point(
-    size = 6, color = "black",
-    aes(shape = GROUP, fill = SPECIES),
-    position = position_jitter(width = 0, height = 0.02)
-  ) +
-  theme_classic() +
-  xlab("PC1 (22.6%)") + ylab("PC2 (3.0%)") +
-  geom_hline(yintercept = 0, linetype = "dashed") +
-  geom_vline(xintercept = 0, linetype = "dashed") +
-  scale_fill_manual("SPECIES", values = c("goldenrod", "brown", "black", "grey")) +
-  scale_shape_manual("GROUP", values = c(25, 21)) +
-  theme(legend.position = "top") +
-  guides(
-    fill = guide_legend(override.aes = list(shape = 21, size = 5, stroke = 0.5)),
-    shape = guide_legend(override.aes = list(fill = NA, size = 5))
-  )
+#Test for normality
+shapiro.test(HET_FILT$HET)
 
-#### 3D plotly: PC1 vs PC2 vs PC3, colored by SPECIES, symbol by GROUP ####
-species_colors <- c(
-  "Tympanuchus cupido" = "goldenrod",
-  "Tympanuchus pallidicinctus" = "brown",
-  "Tympanuchus phasianellus" = "black",
-  "Tympanuchus phasianellus/Tympanuchus cupido" = "grey"
-)
+data:  HET_FILT$HET
+W = 0.84473, p-value = 0.004355
 
-group_symbols <- c("Allopatric" = "circle", "Sympatric" = "diamond")
+#Pairwise test of heterozygosity by ecoregion
+pairwise.wilcox.test(HET_FILT$heterozygosity20x, HET_FILT$GRP, p.adjust.method = "BH")
 
-p <- plot_ly()
+	Pairwise comparisons using Wilcoxon rank sum exact test 
 
-# SPECIES traces
-for (sp in names(species_colors)) {
-  df_sub <- x[x$SPECIES == sp, ]
-  p <- p %>% add_trace(
-    data = df_sub,
-    x = ~V1, y = ~V2, z = ~V3,
-    type = 'scatter3d', mode = 'markers',
-    marker = list(
-      size = 8,
-      color = species_colors[[sp]],
-      symbol = group_symbols[df_sub$GROUP],
-      line = list(color = 'black', width = 1)
-    ),
-    name = sp,
-    legendgroup = "species",
-    legendgrouptitle = list(text = "Species", font = list(color = 'black')),
-    showlegend = TRUE
-  )
-}
+data:  HET_FILT$heterozygosity20x and HET_FILT$GRP 
 
-# Dummy GROUP/symbol traces (legend-only, white fill, black font)
-for (grp in names(group_symbols)) {
-  p <- p %>% add_trace(
-    x = 0, y = 0, z = 0,
-    type = 'scatter3d', mode = 'markers',
-    marker = list(
-      size = 8, color = 'white', symbol = group_symbols[[grp]],
-      line = list(color = 'black', width = 1)
-    ),
-    name = grp,
-    legendgroup = "group",
-    legendgrouptitle = list(text = "Group", font = list(color = 'black')),
-    visible = "legendonly",
-    showlegend = TRUE
-  )
-}
+        Past
+Present 0.53
 
-p <- p %>% layout(
-  scene = list(
-    xaxis = list(title = 'PC1 (22.6%)'),
-    yaxis = list(title = 'PC2 (2.99%)'),
-    zaxis = list(title = 'PC3 (2.13%)'),
-    camera = list(eye = list(x = 1.5, y = 1.5, z = 1.5)),
-    aspectmode = "cube"
-  ),
-  legend = list(
-    groupclick = "toggleitem",
-    font = list(color = 'black')
-  ),
-  margin = list(l = 50, r = 50, b = 50, t = 50)
-)
-
-p
-
-#### Export 3D plot as SVG ####
-save_image(p, path.expand("~/PCA_3D_plot.svg"), width = 1200, height = 900)
+P value adjustment method: BH 
